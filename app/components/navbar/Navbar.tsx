a028c18ac8de35692030950467d95f1f52cfa57c
ighmaz <ighmazcool@gmail.com>
2024-08-07 19:36:02 +0530

    fix pricing navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -69,49 +69,53 @@ const Navbar = () => {
           menuOpen ? "flex" : "hidden"
         } flex-col md:flex-row absolute md:static top-16 left-0 w-full md:w-auto bg-black md:bg-transparent z-20 p-5 md:p-0`}
       >
-        <button
-          onClick={() => router.push("/pricing")}
-          className="text-gray-300 text-lg hover:text-gray-500 mb-4 md:mb-0 md:mr-10"
-        >
-          Pricing
-        </button>
         {user ? (
-          <div className="relative">
-            <button onClick={toggleTooltip} className="focus:outline-none">
-              <img
-                src={user.photoUrl}
-                alt="Profile"
-                className="w-8 h-8 rounded-full"
-              />
-            </button>
-            {tooltipOpen && (
-              <div
-                ref={tooltipRef}
-                className="absolute right-0 mt-2 w-48 bg-gray-700 text-white shadow-lg rounded-lg py-2 z-30"
+          <div className="md:flex">
+            <div>
+              <button
+                onClick={() => router.push("/pricing")}
+                className="text-gray-300 text-lg hover:text-gray-500 mb-4 md:mb-0 md:mr-10 mt-1"
               >
-                <div className="px-4 py-2">
-                  <span className="block font-semibold ">{user.name}</span>
-                  <span className="block text-sm">{user.email}</span>
+                Pricing
+              </button>
+            </div>
+            <div className="relative">
+              <button onClick={toggleTooltip} className="focus:outline-none">
+                <img
+                  src={user.photoUrl}
+                  alt="Profile"
+                  className="w-8 h-8 rounded-full"
+                />
+              </button>
+              {tooltipOpen && (
+                <div
+                  ref={tooltipRef}
+                  className="absolute right-0 mt-2 w-48 bg-gray-700 text-white shadow-lg rounded-lg py-2 z-30"
+                >
+                  <div className="px-4 py-2">
+                    <span className="block font-semibold ">{user.name}</span>
+                    <span className="block text-sm">{user.email}</span>
+                  </div>
+                  <hr className="border-gray-200" />
+                  <button
+                    onClick={() => {
+                      router.push("/pricing");
+                    }}
+                    className="w-full text-left px-4 py-2 hover:bg-gray-800 flex items-center transition duration-300 ease-in-out"
+                  >
+                    <FaCreditCard className="mr-2 text-blue-700" />
+                    Purchase Credit
+                  </button>
+                  <button
+                    onClick={handleLogout}
+                    className="w-full text-left px-4 py-2 hover:bg-red-500 flex items-center transition duration-300 ease-in-out"
+                  >
+                    <FaSignOutAlt className="mr-2 text-red-600" />
+                    Logout
+                  </button>
                 </div>
-                <hr className="border-gray-200" />
-                <button
-                  onClick={() => {
-                    router.push("/pricing");
-                  }}
-                  className="w-full text-left px-4 py-2 hover:bg-gray-800 flex items-center transition duration-300 ease-in-out"
-                >
-                  <FaCreditCard className="mr-2 text-blue-700" />
-                  Purchase Credit
-                </button>
-                <button
-                  onClick={handleLogout}
-                  className="w-full text-left px-4 py-2 hover:bg-red-500 flex items-center transition duration-300 ease-in-out"
-                >
-                  <FaSignOutAlt className="mr-2 text-red-600" />
-                  Logout
-                </button>
-              </div>
-            )}
+              )}
+            </div>
           </div>
         ) : (
           <button
